1d527c83e53915cb5aa3bf7e370319a0aaee8a84
davide-rullo <davide.rullo01@gmail.com>
2023-10-27 16:10:49 +0200

    add layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,18 +16,67 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
 
     <main class="bg-light">
-        <div class="container">
+        <div class="container p-5">
             <div class="row row-cols-1 row-cols-md-2 g-3">
 
                 @forelse ($movies as $movie)
                 <div class="col">
                     <div class="card">
-                        <img src="" class="card-img-top" alt="">
+                        <img src="https://picsum.photos/200/300?random={{ $movie->id }}" class="card-img-top" alt="">
                         <div class="card-body">
-                            <p>{{$movie->title}}</p>
+                            <h3>{{$movie->title}}</h3>
+                            <div class="ot d-flex">
+                                <h5>Original title: {{$movie->original_title}}</h5>
+                            </div>
+
+                            <h5>Nationality: {{$movie->nationality}}
+                            </h5>
+                            <h5>Release date: {{$movie->date}}</h5>
+                            <h5>Vote: {{$movie->vote}}</h5>
+
+
                         </div>
+
                     </div>
                 </div>
                 @empty
